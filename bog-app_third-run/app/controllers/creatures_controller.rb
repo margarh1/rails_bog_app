ccb8e686bc00bad8ac961dca61a8661329ef98c2
margarh1 <ha.margaret1@gmail.com>
2016-12-16 16:49:55 -0800

    finished part one

---
+++ bog-app_third-run/app/controllers/creatures_controller.rb
@@ -0,0 +1,13 @@
+class CreaturesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
---
+++ bog-app_third-run/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  root 'creatures#index'
+
+  get 'creatures/index' => 'creatures#index', as: 'creatures'
+
+  get 'creatures/new' => 'creatures#new', as: 'new_creature'
+  post 'creatures' => 'creatures#create'
+  get 'creatures/edit' => 'creatures#edit', as: 'edit_creature'
+
+  get 'creatures/:id' => 'creatures#show', as: 'creature'
+
+  put 'creatures/:id' => 'creatures#update'
+  patch 'creatures/:id' => 'creatures#update'
+
+  delete 'creatures/:id' => 'creatures#destroy'
+
+end
