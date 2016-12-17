d1f3a690a0ce5d2c24946c38f2dd6ce381185aa8
margarh1 <ha.margaret1@gmail.com>
2016-12-16 16:26:44 -0800

    finished part two

--- bog-app_second-run/app/controllers/creatures_controller.rb
+++ bog-app_second-run/app/controllers/creatures_controller.rb
@@ -4,4 +4,31 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
   end
 
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      redirect_to creature_path(@creature)
+    else
+      redirect_to new_creature_path
+    end
+  end
+
+  def show
+    @creature = Creature.find(creature_id)
+  end
+
+  private
+
+  def creature_id
+    params[:id]
+  end
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
 end
