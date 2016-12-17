f4db13186103068de789243b1c9c90c68ce0baf6
margarh1 <ha.margaret1@gmail.com>
2016-12-16 16:57:39 -0800

    finished first part, I think

--- bog-app_third-run/app/controllers/creatures_controller.rb
+++ bog-app_third-run/app/controllers/creatures_controller.rb
@@ -1,13 +1,36 @@
 class CreaturesController < ApplicationController
   def index
+    @creatures = Creature.all
   end
 
   def new
+    @creature = Creature.new
+  end
+
+  def create
   end
 
   def edit
+    @creature = Creature.find(creature_id)
+  end
+
+  def update
   end
 
   def show
+    @creature = Creature.find(creature_id)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def creature_id
+    params[:id]
+  end
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 end
--- bog-app_third-run/config/routes.rb
+++ bog-app_third-run/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'creatures#index'
 
-  get 'creatures/index' => 'creatures#index', as: 'creatures'
+  get 'creatures' => 'creatures#index', as: 'creatures'
 
   get 'creatures/new' => 'creatures#new', as: 'new_creature'
   post 'creatures' => 'creatures#create'
